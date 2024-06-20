d8177d9a3b3f2cbafe607dadd490d19fdd8400b6
Kyle Smith <kyle@kmaismith.com>
2024-06-20 09:19:44 -0400

    WIP: Implement generics system to implement interfaces

---
+++ common/generics.sh
@@ -0,0 +1,41 @@
+export GENERIC_SERVICE_http="caddy"
+export GENERIC_SERVICE_dns="bind9"
+
+_is_generic() {
+	local _generic="${1}"
+	local -a _generic_services=()
+
+	readarray -t _generic_services < <(
+ 		export | \
+			awk '{print $3}' | \
+			cut -d'=' -f1 | \
+			grep "GENERIC_SERVICE_" | \
+			sed -e 's/GENERIC_SERVICE_//'
+	)
+	echo "Generic Services: ${_generic_services[*]}"
+
+	[[ " ${_generic_services[*]} " == *" ${_generic} "* ]]	
+}
+export -f _is_generic
+
+_set_generic() {
+	local _generic="${1}"
+	local _service="${2-}"
+	_is_generic "${_generic}" || return
+
+	local -n _generic_var="GENERIC_SERVICE_${_generic}"
+	_generic_var="${_service}"
+
+	sed -i "s/^export GENERIC_SERVICE_${_generic}=.*/export GENERIC_SERVICE_${_generic}=\"${_service-}\"/" \
+		"${SERVICES_ROOT}/common/generics.sh"
+}
+export -f _set_generic
+
+_get_generic() {
+	local _generic="${1}"
+	local -n _service="GENERIC_SERVICE_${_generic}"
+
+	echo "${_service}"
+}
+export -f _get_generic
+
