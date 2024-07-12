80e929f6d97a1cb62e9c9c43e43328037851c75f
Kyle Smith <kyle@kmaismith.com>
2024-07-12 18:48:33 -0400

    All services, environments share docker daemon (for now)
    - Add docker::run for non-compose container spin up

--- lib/libdocker.bash
+++ lib/libdocker.bash
@@ -8,19 +8,52 @@ include "<system/sudo>"
 docker::compose() {
 	local docker_socket
 
-	docker_socket="${SYSTEM_ROOT}/$(env_path "${SERVICE_ENV}")/.docker/run/docker.sock"
+	docker_socket="/var/run/docker.sock"
 
 	if [ -w "${docker_socket}" ]; then
 		DOCKER_HOST="unix://${docker_socket}" docker compose \
 			-f "${SERVICE_ROOT}/conf/docker-compose.yaml" \
-			-p "${SERVICE_NAME}" \
+			-p "${SERVICE_ENV}-${SERVICE_NAME}" \
 			"${@}"
 	else
 		consented_sudo "${reason}, docker must be run as root until you reboot your mahine" \
 			DOCKER_HOST="unix://${docker_socket}" docker compose \
 				-f "${SERVICE_ROOT}/conf/docker-compose.yaml" \
-				-p "${SERVICE_NAME}" \
+				-p "${SERVICE_ENV}-${SERVICE_NAME}" \
 				"${@}"
 	fi
 }
 export -f docker::compose
+
+# Expects:
+# - DOCKER_IMAGE="registry/image:tag"
+# - DOCKER_MOUNTS=("/host/path:/container/path" ...)
+# - DOCkER_ENVS=(VAR="value" ...)
+# - DOCKER_PORTS=("host:container" ...)
+# - DOCKER_CMD=("cmd" "arg" ...)
+docker::run() {
+	local -a docker_args=()
+
+	docker_args+=(
+		"--network" "${SERVICE_ENV}"
+		"--name" "${SERVICE_ENV}-${SERVICE_NAME}"
+		"--user" "$(id -u "${SERVICE_ENV}_env"):$(id -g "${SERVICE_ENV}_env")"
+	)
+
+	for mount in "${DOCKER_MOUNTS[@]}"; do
+		docker_args+=("--volume" "${mount}")
+	done
+	
+	for env in "${DOCKER_ENVS[@]}"; do
+		docker_args+=("--env" "${env}")
+	done
+
+	if [ "${SERVICE_ENV}" != "host" ]; then
+		for port in "${DOCKER_PORTS[@]}"; do
+			docker_args+=("--port" "127.0.0.1:${port}")
+		done
+	fi
+
+	docker run "${docker_args[@]}" "${DOCKER_IMAGE}" "${DOCKER_CMD[@]}"
+}
+export -f docker::run
